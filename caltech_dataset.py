c8a2be0ba1a7ea3f44b9996083896c9d941406e4
Alessandro La Rocca <62109901+AlessandroLaRocca96@users.noreply.github.com>
2021-04-18 17:41:17 +0200

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -5,8 +5,10 @@ from PIL import Image
 import os
 import os.path
 import sys
-
-
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, 'rb') as f:
@@ -20,6 +22,16 @@ class Caltech(VisionDataset):
 
         self.split = split # This defines the split you are going to use
                            # (split files are called 'train.txt' and 'test.txt')
+
+
+        self.root = root+"/"+split+".txt" #./root/train.txt
+        self.caltech_frame = pd.read_csv(self.root, delimiter = '\n', header=None)
+        self.transform = transform
+        self.target_transform = target_transform
+
+
+
+      
 
         '''
         - Here you should implement the logic for reading the splits files and accessing elements
@@ -35,12 +47,17 @@ class Caltech(VisionDataset):
         __getitem__ should access an element through its index
         Args:
             index (int): Index
-
         Returns:
             tuple: (sample, target) where target is class_index of the target class.
         '''
+        img_name = os.path.join(self.root, self.caltech_frame.iloc[index, 0])
+        label = img_name.split("/")[3]
+        
+        name = img_name.split("/")[4]
 
-        image, label = ... # Provide a way to access image and label via index
+        image = io.imread("./root/101_ObjectCategories/"+"/"+label+"/"+name)
+        image = Image.fromarray(image)
+        sample = {'image' : image, 'label' : label} # Provide a way to access image and label via index
                            # Image should be a PIL Image
                            # label can be int
 
@@ -48,12 +65,13 @@ class Caltech(VisionDataset):
         if self.transform is not None:
             image = self.transform(image)
 
-        return image, label
+        return sample
 
     def __len__(self):
         '''
         The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
         '''
-        length = ... # Provide a way to get the length (number of elements) of the dataset
+
+        length = self.caltech_frame.size # Provide a way to get the length (number of elements) of the dataset
         return length
